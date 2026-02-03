dbe5372a8f900a8022471f9f0163c49493ec4b2e
Lakamo Samuel <lakamosamuel7@gmail.com>
2026-02-03 15:10:00 +0100

    feat: update aiMessage.ts

--- src/aiMessage.ts
+++ src/aiMessage.ts
@@ -22,7 +22,7 @@ export async function generateCommitMessageAI(): Promise<string> {
 
   try {
     const res = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
